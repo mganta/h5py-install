7974b8b6bae14c41e3fcee07feeb0e1b83a2accd
madhu ganta <madhu@madhus-mbp.lan>
2018-04-02 22:59:10 -0500

    updates

--- h5pyinstall.sh
+++ h5pyinstall.sh
@@ -8,4 +8,6 @@ git clone https://github.com/mganta/h5py-install.git
 
 cd h5py-install
 
-sudo python setup.py install
+#sudo python setup.py install
+sudo /usr/bin/anaconda/bin/conda-build h5spark
+sudo /usr/bin/anaconda/bin/conda install --use-local h5spark
