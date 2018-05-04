c1a32ea9d1ae077a111f0ff0467800e237e331e1
Madhu <madhusagar@gmail.com>
2018-05-04 15:22:59 -0500

    Update perm.sh

--- perm.sh
+++ perm.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-chmod -R 755 /usr/lib/hdinsight-common/certs/key_decryption_cert.prv
+chmod 755 /usr/lib/hdinsight-common
+chmod 755 /usr/lib/hdinsight-common/certs
+chmod 755 /usr/lib/hdinsight-common/certs/key_decryption_cert.prv
